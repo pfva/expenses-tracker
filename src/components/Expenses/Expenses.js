2e85b3dc751a7fe6507d53c74490ca3bf98fef2c
Petter <petter.f.andersson@gmail.com>
2021-06-14 19:48:35 +0200

    Add chart component to display expenses visually

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import ExpensesList from './ExpensesList';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesChart from './ExpensesChart';
 import Card from '../UI/Card';
 import './Expenses.css';
 
@@ -21,6 +22,7 @@ function Expenses(props) {
         onChangeYear={changeYearHandler}
         pickedYear={pickedYear}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList expenses={filteredExpenses} />
     </Card>
   );
